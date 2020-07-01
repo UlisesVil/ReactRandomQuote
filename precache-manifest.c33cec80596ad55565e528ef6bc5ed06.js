17db2e1008bb9e7957d74b1cd397e203d517a12f
github_UlisesVil <ulisesvil5@hotmail.com>
2020-07-01 03:25:16 -0500

    Updates

---
+++ precache-manifest.c33cec80596ad55565e528ef6bc5ed06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827e2c98993bef6e0f18475fb1f1c983",
+    "url": "/ReactRandomQuote/index.html"
+  },
+  {
+    "revision": "ba9230bb0e807aa61a03",
+    "url": "/ReactRandomQuote/static/css/main.2869d687.chunk.css"
+  },
+  {
+    "revision": "f91cf6d3d7148286374f",
+    "url": "/ReactRandomQuote/static/js/2.56fac647.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactRandomQuote/static/js/2.56fac647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9230bb0e807aa61a03",
+    "url": "/ReactRandomQuote/static/js/main.5ffcabd4.chunk.js"
+  },
+  {
+    "revision": "d33153827fb80b8ddb8a",
+    "url": "/ReactRandomQuote/static/js/runtime-main.9fd4a80f.js"
+  },
+  {
+    "revision": "4b957c6dea0180cc2aad1b78acba880a",
+    "url": "/ReactRandomQuote/static/media/castano.4b957c6d.jpg"
+  },
+  {
+    "revision": "0f2095a916b1d9c113068c2158368932",
+    "url": "/ReactRandomQuote/static/media/pap08.0f2095a9.png"
+  }
+]);
